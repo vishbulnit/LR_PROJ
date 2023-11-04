59e27229c3f1aaf628d12cd96a3f4e64c0fce78e
Vishnukumar Vishwakarma <vishwakarma91@gmail.com>
2023-11-04 23:48:32 +0530

    latest version of code

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,6 +1,6 @@
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
-#from src.DimondPricePrediction.components.data_transformation import DataTransformation
-#from src.DimondPricePrediction.components.model_trainer import ModelTrainer
+from src.DimondPricePrediction.components.data_transformation import DataTransformation
+from src.DimondPricePrediction.components.model_trainer import ModelTrainer
 
 import os
 import sys
@@ -10,14 +10,14 @@ import pandas as pd
 
 obj=DataIngestion()
 
-obj.initiate_data_ingestion()
+#obj.initiate_data_ingestion()
 
-#train_data_path,test_data_path=obj.initiate_data_ingestion()
+train_data_path,test_data_path=obj.initiate_data_ingestion()
 
-#data_transformation=DataTransformation()
+data_transformation=DataTransformation()
 
-#train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
+train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
 
 
-#model_trainer_obj=ModelTrainer()
-#model_trainer_obj.initate_model_training(train_arr,test_arr)
+model_trainer_obj=ModelTrainer()
+model_trainer_obj.initate_model_training(train_arr,test_arr)
